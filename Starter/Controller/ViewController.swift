214a66eda7b15ef60fd730bb8605c0f49979c60a
jaavion <jaavion99@gmail.com>
2021-10-10 17:29:05 -0700

    created login page, added create profile

--- Starter/Controller/ViewController.swift
+++ Starter/Controller/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var emailTxtField:UITextField!
+    @IBOutlet var passwordTxtField:UITextField!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.black;
+        self.emailTxtField.placeholder = "email";
+        self.passwordTxtField.placeholder = "password";
     }
-
+    
+    
+    @IBAction func goToMain(_ sender: UIButton!) {
+         
+    }
+    
+    @IBAction func clickCreateProfile(_ sender: UIButton!) {
+        let story = UIStoryboard.init(name: "Main", bundle: nil);
+        let createVC = story.instantiateViewController(identifier: "CreateProfile");
+        self.navigationController?.pushViewController(createVC, animated: true);
+        
+    }
 
 }
 
